a6fc891057dc5552ca6299a7108ee472ebea7fc5
Kei Nomura <k@ozn.pw>
2026-04-24 13:31:56 +0900

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-data'), 'version' => '2d69e2e163ba39e4d849');
+<?php return array('dependencies' => array('wp-blocks', 'wp-data'), 'version' => 'e594fc26f24e4bb73d97');
